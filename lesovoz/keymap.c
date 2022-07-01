0859880a72a98b9e707bf9be081e5f5c20de2296
Yutaka HARA <yutaka.hara+github@gmail.com>
2022-07-01 22:22:15 +0900

    edit lisence

--- lesovoz/keymap.c
+++ lesovoz/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2022 Caleb Lightfoot
+/* Copyright 2022 Yutaka Hara
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
